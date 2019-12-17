e5605278ac7d88ccc1c7fa3812c28e44e95b488e
zhengbo wang <zealband.w@gmail.com>
2019-12-17 11:23:25 +0800

    update

---
+++ api/src/main/java/com/autoleague/forumApplication.java
@@ -0,0 +1,13 @@
+package com.autoleague;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class forumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(forumApplication.class, args);
+    }
+}
